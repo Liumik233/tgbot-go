ec188425105cc8a73f25570057a0469b48828b83
Liumik233 <liumikhazne@gmail.com>
2020-11-27 22:41:31 +0800

    update

--- main.go
+++ main.go
@@ -118,15 +118,15 @@ func main() {
 		return
 	}
 	tb1.Handle(tb.OnAddedToGroup, func(m *tb.Message) {
-		tb1.Send(m.Chat, "欢迎加入新番计划，在群里发送/join 邮箱即可加入团队盘")
+		tb1.Reply(m.ReplyTo, "欢迎加入新番计划，在群里发送/join 邮箱即可加入团队盘")
 	})
 	tb1.Handle(tb.OnText, func(m *tb.Message) {
 		if strings.HasPrefix(m.Text, "/join") {
 			str := strings.Replace(m.Text, " ", "", -1)
 			if cuser(srv, strings.TrimPrefix(str, "/join"), fileid) == 0 {
-				tb1.Reply(m, "添加成功")
+				tb1.Reply(m.ReplyTo, "添加成功")
 			} else {
-				tb1.Reply(m, "添加失败")
+				tb1.Reply(m.ReplyTo, "添加失败")
 			}
 		}
 	})
